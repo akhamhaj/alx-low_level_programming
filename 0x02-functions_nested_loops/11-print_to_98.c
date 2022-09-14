2357a1cff963cdf5d10a2d720043ad4c324699e7
Jude <akhamhajude@outlook.com>
2022-09-13 20:39:55 -0700

    98 bat

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,22 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * add - adds two numbers together.
- * @i: The first number to be added.
- * @j: The second number to be added.
- * Return: the sum of two numbers.
+ * print_to_98 - print all natural numbers from n to 98.
+ * @n: the number to start counting from to 98
+ * Return: void returns nothing.
  */
-
-int add(int i, int j)
+void print_to_98(int n)
 {
-		return (i + j);
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
 }
