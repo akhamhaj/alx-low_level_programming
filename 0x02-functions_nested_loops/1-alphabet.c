65e41a7006f4d2d8af4079743343c2baaa64bba4
Jude <akhamhajude@outlook.com>
2022-09-13 18:25:21 -0700

    1alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,23 +5,13 @@
  *
  *  Return: Always 0 (Succes)
  */
-
 void print_alphabet(void)
-
 {
+	int n;
 
-		int n;
-
-
-
-			for (n = 'a'; n <= 'z'; n++)
-
-					{
-
-								_putchar(n);
-
-									}
-
-				_putchar('\n');
-
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
 }
